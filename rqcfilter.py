7bd749185b40fef71f54b55b8896b36f15925d7b
Adam R. Rivers <adam.rivers@ars.usda.gov>
2017-02-17 16:25:27 -0500

    split core functions into rqcmain module
    Former-commit-id: cb17dccb957cf3e2899330c3a1fa67a654b30dda

--- rqcfilter.py
+++ rqcfilter.py
@@ -8,142 +8,143 @@ import logging
 import tempfile
 import json
 import shutil
+import rqcmain
 
 
-def build_vertebrate_db(cat, dog, human, mouse, datadir):
-    """Builds a bbmap.sh database for mapping reads to masked versions of
-    the cat, dog, human and mouse genome. Returns the path of the database"""
-    try:
-        parameters = ['bbmap.sh', 'build=1', 'k=14', 'usemodulo',
-                      'ref=' +
-                      os.path.abspath(cat) + ',' +
-                      os.path.abspath(dog) + ',' +
-                      os.path.abspath(human) + ',' +
-                      os.path.abspath(mouse),
-                      'path=' + os.path.join(datadir, 'dogcatmousehuman')]
-        subprocess.run(parameters)
-        return os.path.join(datadir, 'dogcatmousehuman')
-    except RuntimeError:
-        print("Couldn't build database of vertebrate contaminants using bbmap")
-
-
-class Fastq():
-
-    @classmethod
-    def parse_params(self):
-        """converts parameters from a json file into a dictionary formatted
-        for use by bbtools"""
-        try:
-            with open("data/parameters.json", 'r') as p:
-                fastq_parameters = json.load(p)
-            bbtoolsdict = {}
-            for key in fastq_parameters["rqcfilter"]:
-                bbtoolsdict[key] = []
-                for key2 in fastq_parameters["rqcfilter"][key]:
-                    if isinstance(fastq_parameters["rqcfilter"][key][key2],
-                                  str):
-                        bbtoolsdict[key].append(str(key2) + "=" +
-                                                str(fastq_parameters
-                                                ["rqcfilter"][key][key2]))
-                    elif isinstance(fastq_parameters["rqcfilter"][key][key2],
-                                    list):
-                        bbtoolsdict[key].append(str(key2) + "=" +
-                                                ",".join(fastq_parameters
-                                                         ["rqcfilter"]
-                                                         [key][key2]))
-            return bbtoolsdict
-        except RuntimeError:
-                print("Could not load and parse the parameters.json file \
-                      correctly")
-
-    def __init__(self, path):
-        self.abspath = os.path.abspath(path)
-        self.filepath, self.filename = os.path.split(os.path.abspath(path))
-        self.metadata = {}
-
-    def __repr__(self):
-        return 'Fastq Class object :' + self.filename
-
-    def filter_contaminants(self, outdir):
-        """Calls bbduk to perform adapter removal"""
-        try:
-            bbtoolsdict = self.parse_params()
-            parameters = ['bbduk.sh',
-                          'in=' + self.abspath,
-                          'out=' + os.path.join(outdir, 'clean1.fq.gz'),
-                          'outduk=' + os.path.join(outdir, 'kmerStats1.txt'),
-                          'stats=' + os.path.join(outdir, 'scaffoldStats1.txt')]
-            parameters.extend(bbtoolsdict['filter_contaminants'])
-            p1 = subprocess.run(parameters, check=True, stderr=subprocess.PIPE)
-            self.metadata['filter_contaminants'] = list(os.walk(outdir))
-            return p1.stderr.decode('utf-8')
-        except RuntimeError:
-            print("could not perform contaminant filtering with bbduk")
-
-    def trim_adaptors(self, outdir):
-        """Calls bbduk to remove contaminant sequences"""
-        try:
-            bbtoolsdict = self.parse_params()
-            parameters = ['bbduk.sh', 'in=' + self.abspath,
-                          'out=' + os.path.join(outdir, 'clean2.fq.gz'),
-                          'stats=' + os.path.join(outdir, 'scaffoldStats2.txt')]
-            parameters.extend(bbtoolsdict['trim_adaptors'])
-            p2 = subprocess.run(parameters, check=True, stderr=subprocess.PIPE)
-            self.metadata['trim_adaptors'] = list(os.walk(outdir))
-            return p2.stderr.decode('utf-8')
-        except RuntimeError:
-            print("could not perform adaptor removal with bbduk")
-
-    def merge_reads(self, outdir):
-        """merge reads to generate insert size histogram and \
-        optionally save"""
-        try:
-            bbtoolsdict = self.parse_params()
-            parameters = ['bbmerge.sh', 'in1=' + self.abspath,
-                          'ihist=' + os.path.join(outdir, 'merge_histogram.txt'),
-                          'outc=' + os.path.join(outdir, 'cardinality.txt'),
-                          'outm=' + os.path.join(outdir, 'merged.fq.gz'),
-                          'outu=' + os.path.join(outdir, 'unmerged.fq.gz')]
-            parameters.extend(bbtoolsdict['merge_reads'])
-            p3 = subprocess.run(parameters, check=True, stderr=subprocess.PIPE)
-            self.metadata['merge_reads'] = list(os.walk(outdir))
-            return p3.stderr.decode('utf-8')
-        except RuntimeError:
-            print("could not perform read merging with bbmerge")
-
-    def remove_vertebrate_contaminants(self, outdir):
-        """maps reads to repeat-masked human, dog, cat and mouse genomes
-        to remove contaminants"""
-        try:
-            bbtoolsdict = self.parse_params()
-            parameters = ['bbmap.sh',
-                          'in=' + self.abspath,
-                          'outu=' + os.path.join(outdir, 'novert.fq.gz')]
-            parameters.extend(bbtoolsdict['remove_vertebrate_contaminants'])
-            p4 = suborocess.run(parameters, check=True, stderr=subprocess.PIPE)
-            self.metadata['remove_vertebrate_contaminants'] = list(os.walk(outdir))
-            return p4.stderr.decode('utf-8')
-        except RuntimeError:
-            print("Could not perform vertebrate conaminant removal with bbmap")
-
-    def sortbyname(self):
-        """Sorts a fastq file by read names, outputs uncompressed fastq"""
-        try:
-            temp_ordered_dir = tempfile.mkdtemp()
-            root, base = os.path.split(self.abspath)
-            if base.endswith(".gz"):
-                uc = base.rsplit(".", 1)[0]
-            else:
-                uc = base
-            tfile = os.path.join(temp_ordered_dir, 'sorted.fq')
-            subprocess.run(['sortbyname.sh',
-                            "in=" + self.abspath,
-                            "out=" + tfile])
-            shutil.move(tfile, os.path.join(root, uc))
-        except RuntimeError:
-            print("could not reorder fastq file by name")
-        finally:
-            shutil.rmtree(temp_ordered_dir)
+# def build_vertebrate_db(cat, dog, human, mouse, datadir):
+#     """Builds a bbmap.sh database for mapping reads to masked versions of
+#     the cat, dog, human and mouse genome. Returns the path of the database"""
+#     try:
+#         parameters = ['bbmap.sh', 'build=1', 'k=14', 'usemodulo',
+#                       'ref=' +
+#                       os.path.abspath(cat) + ',' +
+#                       os.path.abspath(dog) + ',' +
+#                       os.path.abspath(human) + ',' +
+#                       os.path.abspath(mouse),
+#                       'path=' + os.path.join(datadir, 'dogcatmousehuman')]
+#         subprocess.run(parameters)
+#         return os.path.join(datadir, 'dogcatmousehuman')
+#     except RuntimeError:
+#         print("Couldn't build database of vertebrate contaminants using bbmap")
+#
+#
+# class Fastq():
+#
+#     @classmethod
+#     def parse_params(self):
+#         """converts parameters from a json file into a dictionary formatted
+#         for use by bbtools"""
+#         try:
+#             with open("data/parameters.json", 'r') as p:
+#                 fastq_parameters = json.load(p)
+#             bbtoolsdict = {}
+#             for key in fastq_parameters["rqcfilter"]:
+#                 bbtoolsdict[key] = []
+#                 for key2 in fastq_parameters["rqcfilter"][key]:
+#                     if isinstance(fastq_parameters["rqcfilter"][key][key2],
+#                                   str):
+#                         bbtoolsdict[key].append(str(key2) + "=" +
+#                                                 str(fastq_parameters
+#                                                 ["rqcfilter"][key][key2]))
+#                     elif isinstance(fastq_parameters["rqcfilter"][key][key2],
+#                                     list):
+#                         bbtoolsdict[key].append(str(key2) + "=" +
+#                                                 ",".join(fastq_parameters
+#                                                          ["rqcfilter"]
+#                                                          [key][key2]))
+#             return bbtoolsdict
+#         except RuntimeError:
+#                 print("Could not load and parse the parameters.json file \
+#                       correctly")
+#
+#     def __init__(self, path):
+#         self.abspath = os.path.abspath(path)
+#         self.filepath, self.filename = os.path.split(os.path.abspath(path))
+#         self.metadata = {}
+#
+#     def __repr__(self):
+#         return 'Fastq Class object :' + self.filename
+#
+#     def filter_contaminants(self, outdir):
+#         """Calls bbduk to perform adapter removal"""
+#         try:
+#             bbtoolsdict = self.parse_params()
+#             parameters = ['bbduk.sh',
+#                           'in=' + self.abspath,
+#                           'out=' + os.path.join(outdir, 'clean1.fq.gz'),
+#                           'outduk=' + os.path.join(outdir, 'kmerStats1.txt'),
+#                           'stats=' + os.path.join(outdir, 'scaffoldStats1.txt')]
+#             parameters.extend(bbtoolsdict['filter_contaminants'])
+#             p1 = subprocess.run(parameters, check=True, stderr=subprocess.PIPE)
+#             self.metadata['filter_contaminants'] = list(os.walk(outdir))
+#             return p1.stderr.decode('utf-8')
+#         except RuntimeError:
+#             print("could not perform contaminant filtering with bbduk")
+#
+#     def trim_adaptors(self, outdir):
+#         """Calls bbduk to remove contaminant sequences"""
+#         try:
+#             bbtoolsdict = self.parse_params()
+#             parameters = ['bbduk.sh', 'in=' + self.abspath,
+#                           'out=' + os.path.join(outdir, 'clean2.fq.gz'),
+#                           'stats=' + os.path.join(outdir, 'scaffoldStats2.txt')]
+#             parameters.extend(bbtoolsdict['trim_adaptors'])
+#             p2 = subprocess.run(parameters, check=True, stderr=subprocess.PIPE)
+#             self.metadata['trim_adaptors'] = list(os.walk(outdir))
+#             return p2.stderr.decode('utf-8')
+#         except RuntimeError:
+#             print("could not perform adaptor removal with bbduk")
+#
+#     def merge_reads(self, outdir):
+#         """merge reads to generate insert size histogram and \
+#         optionally save"""
+#         try:
+#             bbtoolsdict = self.parse_params()
+#             parameters = ['bbmerge.sh', 'in1=' + self.abspath,
+#                           'ihist=' + os.path.join(outdir, 'merge_histogram.txt'),
+#                           'outc=' + os.path.join(outdir, 'cardinality.txt'),
+#                           'outm=' + os.path.join(outdir, 'merged.fq.gz'),
+#                           'outu=' + os.path.join(outdir, 'unmerged.fq.gz')]
+#             parameters.extend(bbtoolsdict['merge_reads'])
+#             p3 = subprocess.run(parameters, check=True, stderr=subprocess.PIPE)
+#             self.metadata['merge_reads'] = list(os.walk(outdir))
+#             return p3.stderr.decode('utf-8')
+#         except RuntimeError:
+#             print("could not perform read merging with bbmerge")
+#
+#     def remove_vertebrate_contaminants(self, outdir):
+#         """maps reads to repeat-masked human, dog, cat and mouse genomes
+#         to remove contaminants"""
+#         try:
+#             bbtoolsdict = self.parse_params()
+#             parameters = ['bbmap.sh',
+#                           'in=' + self.abspath,
+#                           'outu=' + os.path.join(outdir, 'novert.fq.gz')]
+#             parameters.extend(bbtoolsdict['remove_vertebrate_contaminants'])
+#             p4 = suborocess.run(parameters, check=True, stderr=subprocess.PIPE)
+#             self.metadata['remove_vertebrate_contaminants'] = list(os.walk(outdir))
+#             return p4.stderr.decode('utf-8')
+#         except RuntimeError:
+#             print("Could not perform vertebrate conaminant removal with bbmap")
+#
+#     def sortbyname(self):
+#         """Sorts a fastq file by read names, outputs uncompressed fastq"""
+#         try:
+#             temp_ordered_dir = tempfile.mkdtemp()
+#             root, base = os.path.split(self.abspath)
+#             if base.endswith(".gz"):
+#                 uc = base.rsplit(".", 1)[0]
+#             else:
+#                 uc = base
+#             tfile = os.path.join(temp_ordered_dir, 'sorted.fq')
+#             subprocess.run(['sortbyname.sh',
+#                             "in=" + self.abspath,
+#                             "out=" + tfile])
+#             shutil.move(tfile, os.path.join(root, uc))
+#         except RuntimeError:
+#             print("could not reorder fastq file by name")
+#         finally:
+#             shutil.rmtree(temp_ordered_dir)
 
 
 def myparser():
@@ -174,11 +175,6 @@ def myparser():
     parser.add_argument('--paired', '-p', action='store_true', default=False,
                         help='A flag to specify if the fastq file is \
                         paired and interleaved. Default = True.')
-
-    parser.add_argument('--keepmerged', '-km', action='store_true',
-                        default=False,
-                        help='Keep fastq files with merged and unmerged reads. \
-                        Defualt = True. and unmerged reads will be output')
 
     args = parser.parse_args()
     return args
@@ -219,47 +215,50 @@ def main():
     # Remove contaminants
     tmp_fc = mk_temp_dir(rqctempdir, 'filter_contaminants')  # make temp dir
     logging.info('Starting contaminant removal')
-    data1 = Fastq(path=abs_fastq)
+    data1 = rqcmain.Fastq(path=abs_fastq)
     deconfiles = data1.filter_contaminants(outdir=tmp_fc)
     logging.info(deconfiles)
+
     # Trim adaptors
     tmp_ta = mk_temp_dir(rqctempdir, 'trim_adaptors')  # make temp. dir.
     logging.info('Starting adaptor trimming')
-    data2 = Fastq(os.path.join(tmp_fc, 'clean1.fq.gz'))
+    data2 = rqcmain.Fastq(os.path.join(tmp_fc, 'clean1.fq.gz'))
     trimfiles = data2.trim_adaptors(outdir=tmp_ta)
     logging.info(trimfiles)
+
+    # Remove vertebrate contaminants
     if args.removevertebrates:
         tmp_rvc = mk_temp_dir(rqctempdir, 'remove_vertebrate_contaminants')
         logging.info('Removing dog, cat, mouse and humanreads')
         # Create new Fastq object
-        data3 = Fastq(os.path.join(tmp_ta, 'clean2.fq.gz'))
+        data3 = rqcmain.Fastq(os.path.join(tmp_ta, 'clean2.fq.gz'))
         if not os.path.isdir(data/dogcatmousehuman/ref):
             pass  # pass until this is on the server with the references
-            # build_vertebrate_db(cat=data/cat.fa.gz,
+            # rqcmain.build_vertebrate_db(cat=data/cat.fa.gz,
             #                     dog=data/dog.fa.gz,
             #                     human=data/hg19.fa.gz,
             #                     mouse=data/mouse.fa.gz,
             #                     datadir=data/dogcatmousehuman
-        rvcfiles = data.remove_vertebrate_contaminants(outdir=tmp_rvc)
-        logginf.info(rvcfiles)
+        rvcfiles = data3.remove_vertebrate_contaminants(outdir=tmp_rvc)
+        logging.info(rvcfiles)
 
+    # Merge files
     if args.paired:
-        # merge files
         tmp_mr = mk_temp_dir(rqctempdir, 'merge_reads')  # make temp. dir.
         logging.info('Merging read pairs')
-
-        # Create new Fastq object
+        # determine iput file
         if args.removevertebrates:
             infile = os.path.join(tmp_vcr, 'novert.fq.gz')
         else:
             infile = os.path.join(tmp_ta,'clean2.fq.gz')
-        data4 = Fastq(os.path.join(tmp_mr, infile))
+
+        data4 = rqcmain.Fastq(os.path.join(tmp_mr, infile))
         mergefiles = data4.merge_reads(outdir=tmp_mr)
         logging.info(mergefiles)
 
-    # For now copy all files from the tempdir to the output dir
-    logging.info("copying files from temporary directory to ouput directory")
-    shutil.copytree(rqctempdir, os.path.join(args.output,"output"))
+    # For now, copy all files from the tempdir to the output dir
+    logging.info("Copying files from temporary directory to ouput directory")
+    shutil.copytree(rqctempdir, os.path.join(args.output, "output"))
     shutil.rmtree(rqctempdir)
     logging.info("Completed RQC run")
 
--- test_rqcmain.py
+++ test_rqcmain.py
@@ -3,7 +3,7 @@ import os
 import tempfile
 import filecmp
 import sys
-import rqcfilter
+import rqcmain
 import shutil
 
 class TestfastqMethods(unittest.TestCase):
@@ -11,13 +11,13 @@ class TestfastqMethods(unittest.TestCase):
     def test_filter_contaminants(self):
         try:
             testdir = tempfile.mkdtemp()
-            obj = rqcfilter.Fastq('data/reads.fq.gz')  # create fastq object
+            obj = rqcmain.Fastq('data/reads.fq.gz')  # create fastq object
             obj.filter_contaminants(testdir)  # run method
             fq1 = os.path.join(testdir, 'clean1.fq.gz')  # test output datadir
-            fqo1 = rqcfilter.Fastq(fq1)  # create fastq object from output fastq
+            fqo1 = rqcmain.Fastq(fq1)  # create fastq object from output fastq
             fqo1.sortbyname()  # sort by read name (read order from bbduk varies)
             fq2 = 'tests/outputs/filter_contaminants/clean1.fq'  # valid. data
-            fqo2 = rqcfilter.Fastq(fq2)  # create object with validation data
+            fqo2 = rqcmain.Fastq(fq2)  # create object with validation data
             fqo2.sortbyname()  # sort validation data by name
             #shutil.copytree(testdir, 'fc_dir')
             self.assertTrue(filecmp.cmp(os.path.join(testdir, 'clean1.fq'), fq2))  # test the outputs are the same
@@ -30,13 +30,13 @@ class TestfastqMethods(unittest.TestCase):
     def test_trim_adaptors(self):
         try:
             testdir2 = tempfile.mkdtemp()
-            obj = rqcfilter.Fastq('data/reads.fq.gz')  # create fastq object
+            obj = rqcmain.Fastq('data/reads.fq.gz')  # create fastq object
             obj.trim_adaptors(testdir2)  # run method
             fq1 = os.path.join(testdir2, 'clean2.fq.gz')  # test output ddir
-            fqo1 = rqcfilter.Fastq(fq1)  # create fastq object from output fastq
+            fqo1 = rqcmain.Fastq(fq1)  # create fastq object from output fastq
             fqo1.sortbyname()  # sort by read name (read order from bbduk varies)
             fq2 = 'tests/outputs/trim_adaptors/clean2.fq'  # valid. data
-            fqo2 = rqcfilter.Fastq(fq2)  # create object with validation data
+            fqo2 = rqcmain.Fastq(fq2)  # create object with validation data
             fqo2.sortbyname()  # sort validation data by name
             self.assertTrue(filecmp.cmp(os.path.join(testdir2, "clean2.fq"), fq2))  # test the outputs are the same
         except RuntimeError:
@@ -47,23 +47,23 @@ class TestfastqMethods(unittest.TestCase):
     def test_merge_reads(self):
         try:
             testdir3 = tempfile.mkdtemp()
-            obj = rqcfilter.Fastq('data/reads.fq.gz')  # create fastq object
+            obj = rqcmain.Fastq('data/reads.fq.gz')  # create fastq object
             obj.merge_reads(testdir3)  # run method
             # sort merged reads from test
             fqm1 = os.path.join(testdir3, 'merged.fq.gz')
-            fqmo1 = rqcfilter.Fastq(fqm1)
+            fqmo1 = rqcmain.Fastq(fqm1)
             fqmo1.sortbyname()
             # sort unmerged reads from test
             fqu1 = os.path.join(testdir3, 'unmerged.fq.gz')
-            fquo1 = rqcfilter.Fastq(fqu1)
+            fquo1 = rqcmain.Fastq(fqu1)
             fquo1.sortbyname()
             # sort merged reads from validation data
             fqm2 = 'tests/outputs/merge_reads/merged.fq'  # valid. data
-            fqmo2 = rqcfilter.Fastq(fqm2)  # create object with validation data
+            fqmo2 = rqcmain.Fastq(fqm2)  # create object with validation data
             fqmo2.sortbyname()  # sort validation data by name
             # sort unmerged reads from validation data
             fqu2 = 'tests/outputs/merge_reads/unmerged.fq'  # valid. data
-            fquo2 = rqcfilter.Fastq(fqu2)  # create object with validation data
+            fquo2 = rqcmain.Fastq(fqu2)  # create object with validation data
             fquo2.sortbyname()  # sort validation data by name
             # compare files
             self.assertTrue(filecmp.cmp(os.path.join(testdir3, "merged.fq"),
